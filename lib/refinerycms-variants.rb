cce890465ae5422f2fce584a5b9171b8caf20d64
James Thompson <james.b.thompson@gmail.com>
2012-03-19 15:51:17 +0200

    trying to hook variants into the products gem

--- lib/refinerycms-variants.rb
+++ lib/refinerycms-variants.rb
@@ -15,6 +15,11 @@ module Refinery
         app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
       end
 
+      # LineItem is found in the products gem which this gem relies on
+      initializer "add variants to line items" do
+        LineItem.class_eval { belongs_to :variant}
+      end
+
       config.after_initialize do
         Refinery::Plugin.register do |plugin|
           plugin.name = "variants"
