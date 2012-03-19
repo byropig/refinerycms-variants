5b9e75f4829629d1c19371b75e31112402217ce3
James Thompson <james.b.thompson@gmail.com>
2012-03-19 17:21:03 +0200

    stupid fucking config.to_prepare is not working like it should...

--- lib/refinerycms-variants.rb
+++ lib/refinerycms-variants.rb
@@ -2,40 +2,6 @@ require 'refinerycms-base'
 
 module Refinery
   module Variants
-
-    class << self
-      attr_accessor :root
-      def root
-        @root ||= Pathname.new(File.expand_path('../../', __FILE__))
-      end
-    end
-
-    class Engine < Rails::Engine
-      initializer "static assets" do |app|
-        app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
-      end
-
-      config.to_prepare do
-        LineItem.send :include, Refinery::Variants::LineItemExtender
-        Product.send :include, Refinery::Variants::ProductExtender
-      end
-
-      config.after_initialize do
-        Refinery::Plugin.register do |plugin|
-          plugin.name = "variants"
-          plugin.pathname = root
-          plugin.activity = {
-            :class => Variant,
-            :title => 'name'
-          }
-        end
-      end
-    end
-  end
-end
-
-module Refinery
-  module Variants
     module ProductExtender
       def self.included(model)
         model.send :include, MethodsToCall
@@ -65,3 +31,39 @@ module Refinery
     end
   end
 end
+
+module Refinery
+  module Variants
+
+    class << self
+      attr_accessor :root
+      def root
+        @root ||= Pathname.new(File.expand_path('../../', __FILE__))
+      end
+    end
+
+    class Engine < Rails::Engine
+
+      config.to_prepare do
+        LineItem.send :include, Refinery::Variants::LineItemExtender
+        Product.send :include, Refinery::Variants::ProductExtender
+      end
+
+      initializer "static assets" do |app|
+        app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
+      end
+
+      config.after_initialize do
+        Refinery::Plugin.register do |plugin|
+          plugin.name = "variants"
+          plugin.pathname = root
+          plugin.activity = {
+            :class => Variant,
+            :title => 'name'
+          }
+        end
+      end
+    end
+  end
+end
+
